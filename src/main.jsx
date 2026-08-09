e06a6e4d3f9fc490f728f469dd73d163e0a8d380
moneyfund <138159325+moneyfund@users.noreply.github.com>
2026-08-09 14:51:59 -0600

    Initialize authentication provider

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
+import { AuthProvider } from './auth/AuthContext';
 import './styles/index.css';
 import './styles/navigation.css';
 import './styles/pdfControls.css';
@@ -9,7 +10,9 @@ import './styles/pdfControls.css';
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <AuthProvider>
+        <App />
+      </AuthProvider>
     </BrowserRouter>
   </React.StrictMode>,
 );
