8b4b82c3ef6f37f3605cbf3bdf897c492cba88c1
moneyfund <138159325+moneyfund@users.noreply.github.com>
2026-08-09 19:26:55 -0600

    Isolate platform admin from tenant resolution

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import { AuthProvider } from './auth/AuthContext';
-import { TenantProvider } from './tenants/TenantContext';
 import './styles/index.css';
 import './styles/navigation.css';
 import './styles/pdfControls.css';
@@ -16,9 +15,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
       <AuthProvider>
-        <TenantProvider>
-          <App />
-        </TenantProvider>
+        <App />
       </AuthProvider>
     </BrowserRouter>
   </React.StrictMode>,
